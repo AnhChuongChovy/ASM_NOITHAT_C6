e719f8531fd5bfda8d5f7b279b1178f1a82d0ced
AnhChuongChovy <chuonggo51@gmail.com>
2024-08-02 17:43:21 +0700

    Thêm chức năng thêm giỏ hàng, Thêm HinhAnh vào Account

--- API_NoiThat/Migrations/ApplicationDBContextModelSnapshot.cs
+++ API_NoiThat/Migrations/ApplicationDBContextModelSnapshot.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using API_NoiThat.Data;
 
 namespace WebAPI.Migrations
 {
