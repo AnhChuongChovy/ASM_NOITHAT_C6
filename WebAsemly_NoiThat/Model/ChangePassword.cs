df1da5f67bc07da7177f0909a01d191e77b47b90
HuynhQuyTruong <truonghqpc05828@fpt.edu.vn>
2024-08-03 20:07:38 +0700

    lấy code chương

---
+++ WebAsemly_NoiThat/Model/ChangePassword.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAsemly_NoiThat.Model
+{
+    public class ChangePassword
+    {
+        public int AccountId { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
